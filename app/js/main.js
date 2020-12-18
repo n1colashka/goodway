48e3c245806ae3aa28f526a3f3e420f8724f7ce6
Nick <deckards2505@gmail.com>
2020-12-18 13:41:58 +0600

    feat: first screen add

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function initMenu() {
-        const menuBtn = document.querySelector('.header__btn');
-        const menuWrapper = document.querySelector('.profile-menu');
+        const menuBtn = document.querySelector('.menu__btn');
+        const menuCloseBtn = document.querySelector('.menu__close');
+        const html = document.querySelector('html');
+        const menuWrapper = document.querySelector('.menu');
 
         document.body.addEventListener('click', event => {
-            if (event.target.closest('.profile-menu')) {
-                menuWrapper.classList.add('active');
-                menuBtn.classList.add('active');
-            } else if (event.target.closest('.header__btn')) {
-                menuWrapper.classList.toggle('active');
-                menuBtn.classList.toggle('active');
-            } else {
-                menuWrapper.classList.remove('active');
-                menuBtn.classList.remove('active');
-            }
+            if (event.target.closest('.menu__btn')) {
+                menuWrapper.classList.add('open');
+                // html.classList.add('overflow-hidden');
+            } else if (event.target.closest('.menu__close') || (!event.target.closest('.menu'))){
+                menuWrapper.classList.remove('open');
+                // html.classList.remove('overflow-hidden');
+            } 
         });
     }
 
     // Функции работающие только на мобильных устройствах
-    if (window.innerWidth <= 768) {
-        changePageType();
-        initReadMore();
+    if (window.innerWidth <= 1250) {
+        initMenu();
     }
 
     AOS.init();
-    // initMenu();
 
 
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -2,27 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   function initMenu() {
-    var menuBtn = document.querySelector('.header__btn');
-    var menuWrapper = document.querySelector('.profile-menu');
+    var menuBtn = document.querySelector('.menu__btn');
+    var menuCloseBtn = document.querySelector('.menu__close');
+    var html = document.querySelector('html');
+    var menuWrapper = document.querySelector('.menu');
     document.body.addEventListener('click', function (event) {
-      if (event.target.closest('.profile-menu')) {
-        menuWrapper.classList.add('active');
-        menuBtn.classList.add('active');
-      } else if (event.target.closest('.header__btn')) {
-        menuWrapper.classList.toggle('active');
-        menuBtn.classList.toggle('active');
-      } else {
-        menuWrapper.classList.remove('active');
-        menuBtn.classList.remove('active');
+      if (event.target.closest('.menu__btn')) {
+        menuWrapper.classList.add('open'); // html.classList.add('overflow-hidden');
+      } else if (event.target.closest('.menu__close') || !event.target.closest('.menu')) {
+        menuWrapper.classList.remove('open'); // html.classList.remove('overflow-hidden');
       }
     });
   } // Функции работающие только на мобильных устройствах
 
 
-  if (window.innerWidth <= 768) {
-    changePageType();
-    initReadMore();
+  if (window.innerWidth <= 1250) {
+    initMenu();
   }
 
-  AOS.init(); // initMenu();
+  AOS.init();
 });
